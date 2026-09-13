0171f6f434674824a6d8b8161cf37bc9013099aa
Divya Angelina <divyaangelina@your-moms-MacBook-Air.local>
2026-09-13 14:58:20 -0400

    Fix dark mode toggle

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-```js
-const toggle = document.getElementById("themeToggle");
+const toggle=document.getElementById("themeToggle");
 
-// Dark mode is the default
-document.body.classList.add("dark");
-
-function updateIcon() {
-  toggle.textContent = document.body.classList.contains("dark") ? "☾" : "☼";
+function updateIcon(){
+  toggle.textContent=document.body.classList.contains("dark")?"☾":"☼";
 
   toggle.setAttribute(
     "aria-label",
-    document.body.classList.contains("dark")
-      ? "Switch to light mode"
-      : "Switch to dark mode"
+    document.body.classList.contains("dark") ? "Switch to light mode" : "Switch to dark mode"
   );
 }
 
 updateIcon();
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click",()=>{
   document.body.classList.toggle("dark");
   updateIcon();
 });
-```
